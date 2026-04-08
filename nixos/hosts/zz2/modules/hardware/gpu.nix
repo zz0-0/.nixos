a51bcfe694574df9c4079b9218b2d0d8263a072c
zz0-0 <zz11009988@outlook.com>
2026-04-08 21:07:44 +1200

    Update gpu.nix

--- nixos/hosts/zz2/modules/hardware/gpu.nix
+++ nixos/hosts/zz2/modules/hardware/gpu.nix
@@ -17,7 +17,7 @@
     nvidiaSettings = true;
     modesetting.enable = true;
     prime = {
-      offload.enable = false;
+      offload.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
     };
