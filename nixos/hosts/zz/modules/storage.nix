e84366b222a67e09c264518325e92e74c6b55360
zz0-0 <zz11009988@outlook.com>
2026-04-08 13:10:31 +1200

    Disable powertop in zz and zz2 storage modules

--- nixos/hosts/zz/modules/storage.nix
+++ nixos/hosts/zz/modules/storage.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  powerManagement.powertop.enable = true;
-
   fileSystems."/mnt/drive" = {
     device = "/dev/disk/by-uuid/98427079-47ad-495a-b58f-ac3a155ea1ce";
     fsType = "ext4";
--- nixos/hosts/zz2/modules/storage.nix
+++ nixos/hosts/zz2/modules/storage.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  powerManagement.powertop.enable = true;
-
   # NOTE: Update this with your actual disk UUIDs after running nixos-generate-config on zz2
   # fileSystems."/mnt/drive" = {
   #   device = "/dev/disk/by-uuid/YOUR-UUID-HERE";
