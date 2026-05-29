1bccfd7b92258adcfcb0e2d0739b6e1e47e60976
zz0-0 <zz11009988@outlook.com>
2026-05-29 13:58:11 +1200

    Remove commented-out PulseAudio bluetooth config

--- nixos/shared/bluetooth.nix
+++ nixos/shared/bluetooth.nix
@@ -6,21 +6,6 @@
     "bluetooth"
   ];
 
-  # hardware.pulseaudio = {
-  #   enable = true;
-  #   package = pkgs.pulseaudioFull;
-  #   configFile = pkgs.writeText "default.pa" ''
-  #     load-module module-switch-on-connect
-  #     load-module module-bluetooth-policy
-  #     load-module module-bluetooth-discover
-  #     ## module fails to load with
-  #     ##   module-bluez5-device.c: Failed to get device path from module arguments
-  #     ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-  #     # load-module module-bluez5-device
-  #     # load-module module-bluez5-discover
-  #   '';
-  # };
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
